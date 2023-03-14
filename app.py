7d4be527e69a79bf2d72dbbe5c815a198f91febf
shishirhebbar1 <114677714+shishirhebbar1@users.noreply.github.com>
2023-03-14 22:16:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TEST GPT",
+    page_icon="👋",
+)
+
+st.write("# TEST GPT 👋")
+
+st.write("MEET YOUR PERSONAL CHAT BOT - POWERED BY A COMPLEX AI MODEL")
+st.write("CLICK ON TEST GPT TO ACCESS THE CHAT BOT AND GET STARTED WITH OUR SERVICES")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    
+"""
+)
---
+++ pages/TEST_GPT.py
@@ -0,0 +1,18 @@
+import os
+import openai
+import streamlit as st
+from Google import Create_Service
+import base64
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+st.title("TEST GPT")
+# Load your API key from an environment variable or secret management service
+openai.api_key ="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"
+prompt=st.text_input("WHAT WOULD YOU LIKE US TO DO : ")
+if st.button('CLICK'):
+    response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=1000)
+    a=response["choices"]
+    b=a[0]
+    st.write(b["text"])
+    
+
