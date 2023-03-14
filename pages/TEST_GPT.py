158989d7fcc798444c22351f82beb0d853658f55
shishirhebbar1 <114677714+shishirhebbar1@users.noreply.github.com>
2023-03-14 22:59:29 +0530

    Update TEST_GPT.py

--- pages/TEST_GPT.py
+++ pages/TEST_GPT.py
@@ -3,7 +3,7 @@ import openai
 import streamlit as st
 st.title("TEST GPT")
 # Load your API key from an environment variable or secret management service
-openai.api_key ="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"
+openai.api_key ="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"
 prompt=st.text_input("WHAT WOULD YOU LIKE US TO DO : ")
 if st.button('CLICK'):
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=4000)
